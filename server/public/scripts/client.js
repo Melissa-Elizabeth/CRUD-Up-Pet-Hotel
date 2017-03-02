cb5c7ac33c388e754b3a3f15e07cc536484f7086
Arrielle Kooiman <arrielle.kooiman@gmail.com>
2017-03-02 16:28:21 -0600

    adding get client side code

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,8 +10,8 @@ $.ajax({
     $('#petsTableBody').empty(); // clears the pets currently in the table
     for (var i = 0; i < response.length; i++) { //Loops through the pets array (the response array)
       var currentPetInfo = response[i]; //More legible for code below
-      var $newPetInfo = $('<tr>'); //create a new row for each book
-      $newPetInfo.data('id', currentBook.id); //adds data ID to the new book object so we can call it later
+      var $newPetInfo = $('<tr>'); //create a new row for each pet info
+      $newPetInfo.data('id', currentBook.id); //adds data ID to the pet object so we can call it later
       $newPetInfo.append('<td><input value="' + currentPetInfo.owner + '" class="petOwner"></td>'); //show user the title
       $newPetInfo.append('<td><input value="' + currentPetInfo.pet + '" class="petName"></td>'); //show user the author
       $newPetInfo.append('<td><input value="' + currentPetInfo.breed + '" class="petBreed"></td>'); //show user the edition
