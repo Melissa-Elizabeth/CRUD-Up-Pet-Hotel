78213fb031d66fd9a69d44639dde15cbcf4c5521
Arrielle Kooiman <arrielle.kooiman@gmail.com>
2017-03-03 14:38:34 -0600

    Updating duplicate files?

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 console.log('Starting up the server');
-
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var port = 5000;
 
-app.use(express.static('server/public'));
 
+
+
+app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 var pg = require('pg');
@@ -18,7 +19,59 @@ var config = {
   idleTimeoutMillis: 30000 // 30 seconds to try to connect
 };
 
+
+app.post('/pet/new', function(req, res){
+  // This will be replaced with an INSERT statement to SQL
+  var newPet = req.body;
+
+  pool.connect(function(errorConnectingToDatabase, client, done){
+    if(errorConnectingToDatabase) {
+      // There was an error connecting to the database
+      console.log('Error connecting to database: ', errorConnectingToDatabase);
+      res.sendStatus(500);
+    } else {
+      // We connected to the database!!!
+      // Now, we're gonna' git stuff!!!!!
+      client.query('INSERT INTO pets (pet_name, pet_breed, pet_color) VALUES ($1, $2, $3);',
+      [newPet.petName, newPet.breed, newPet.color],
+      function(errorMakingQuery, result){
+        done();
+        if(errorMakingQuery) {
+          console.log('Error making the database query: ', errorMakingQuery);
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(201);
+        }
+      });
+    }
+  });
+});
+
+
 var pool = new pg.Pool(config);
+
+
+app.post('/owner', function(req, res){
+  var newOwner = req.body;
+  pool.connect(function(err, client, done){
+    if(err) {
+      console.log('Error connecting to database: ', err);
+      res.sendStatus(500);
+    } else {
+      client.query('INSERT INTO owner (first_name, last_name) VALUES ($1, $2);',
+      [newOwner.first_name, newOwner.last_name],
+      function(err, result){
+        done();
+        if(err) {
+          console.log('Error making the database query: ', err);
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(201);
+        }
+      });
+    }
+  });
+}); // end owner post
 
 app.listen(port, function() {
   console.log('We are running on port: ', port);
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $(document).ready function({
 
 //creating a function that gets data from the server and adds it to the petsTable table.
@@ -24,3 +25,49 @@ $.ajax({
 });//ends GET ajax
 }//end getDataAddToTable
 });
+=======
+$(document).ready(function() {
+    console.log('jquery was correctly sourced!');
+    $('#ownerRegisterButton').on('click', function() {
+        console.log('yay, way to click!');
+        var newOwner = {};
+        var newOwnerFirstName;
+        var newOwnerLastName;
+        newOwner.newOwnerFirstName = $('#ownerFirstName').val();
+        newOwner.newOwnerLastName = $('#ownerLastName').val();
+        console.log(newOwner);
+        $.ajax({
+            type: 'POST',
+            url: '/owner',
+            data: newOwner,
+            success: function(response) {
+                console.log(response);
+            } //end success
+        }); //end ajax
+    }); // end click
+
+
+    $('#addPetButton').on('click', addPetClicked);
+
+    function addPetClicked() {
+        console.log('you rock as far as clicking goes!');
+        var newPetObject = {
+            petName: $('#petName').val(),
+            breed: $('#breed').val(),
+            color: $('#color').val()
+        }; // end object
+        console.log('newPetObject', newPetObject);
+        $.ajax({
+            type: 'POST',
+            url: '/pet/new',
+            data: newPetObject,
+            success: function(data) {
+                console.log('new pet post', data);
+            }, // end of success
+            error: function(error) {
+                console.log('error dude');
+            } //end of error
+        }); // end of ajax
+    } // end of addPetClicked function
+}); // end of doc ready
+>>>>>>> 87c16e1d71bbc2cee59feb9ec2667ead70062afd
